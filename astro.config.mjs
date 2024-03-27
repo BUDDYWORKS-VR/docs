5819d03385832274a6916d3fbcfb8ea2de2d47bf
JustBuddy <8122264+JustBuddy@users.noreply.github.com>
2024-03-27 22:10:39 +0100

    Change

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://justbuddy.github.io',
+	site: 'https://docs.buddyworks.wtf',
 	integrations: [
 		starlight({
 			title: 'My Docs',
